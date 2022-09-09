24367a6b1e9c4f7dbb23feaaf51b03d9044c97b3
Felipe <machadofelipe2016@outlook.com>
2022-09-09 11:02:19 -0300

    c2-adding more an interesting thing here

--- index.js
+++ index.js
@@ -84,11 +84,30 @@ const products = [
     },
   ];
 
+  const categories = [
+    {
+      id: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
+      name: "Kitchen",
+    },
+    {
+      id: "34115aac-0ff5-4859-8f43-10e8db23602b",
+      name: "Garden",
+    },
+    {
+      id: "d914aec0-25b2-4103-9ed8-225d39018d1d",
+      name: "Sports",
+    },
+  ];
+  
+
 
 const typeDefs = gql`
  type Query {
    products:[Product]!,
-   product(id:ID): Product
+   product(id:ID): Product,
+   categories: [Category!]!,
+   category(id: ID!): Category
+
  }
   
   type Product{
@@ -101,11 +120,16 @@ const typeDefs = gql`
 
   }
 
+  type Category{
+    id: ID,
+    name: String
+  }
+
 `
 
 const resolvers = {
     Query: {
-       products:() => products ,
+       products:(parent, args, context) => products ,
        product: (parent, args, context) => {
         const ProductId = args.id;
         const product = products.find( product => product.id === ProductId )
@@ -113,7 +137,14 @@ const resolvers = {
         if(!product) return null;
         return product
         
-       }
+       },
+
+       categories: (parent, args, context) =>  categories,
+       category: (parent, args, context) => {
+        const {id} = args ;
+        return categories.find((category) => category.id === id)
+        
+       } 
     },
 
 
